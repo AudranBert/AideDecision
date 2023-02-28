e18ad6b5adccfdaf077aca6dbbc05bb0f74894e2
Jacob Michalski <jacob.michalski@aiway.fr>
2023-02-28 11:20:37 +0100

    add get_vote_distribution

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import csv
-import AideDecision.vote as vote
+import vote
 
 def load_csv(filename):
     with open(filename, "r") as file:
@@ -8,9 +8,10 @@ def load_csv(filename):
         votes = [[] for i in range(len(data[0]))]
         for pref in data:
             for i, vote in enumerate(pref):
-                votes[i].append(vote)
+                votes[i].append(int(vote))
     return votes
 
 if __name__ == "__main__":
-    data = load_csv("profil1.csv")   
-    print(data)
+    data = load_csv("profil1.csv")
+    result = vote.get_votes_distribution(data)
+    print(result)
--- vote.py
+++ vote.py
@@ -1,4 +1,5 @@
-import csv
-
-class one_turn_vote():
-    pass
+def get_votes_distribution(voters, rank=0):
+    distribution = [0 for i in range(len(voters[0]))]
+    for pref in voters:
+        distribution[pref[0] - 1] += 1
+    return distribution
