30fe3a06432893e685ae22178045d90ffed475bb
Audran Bert <57795315+AudranBert@users.noreply.github.com>
2023-02-28 12:19:06 +0100

    add: alternative vote

--- main.py
+++ main.py
@@ -11,7 +11,18 @@ def load_csv(filename):
                 votes[i].append(int(vote))
     return votes
 
+
+def print_result(vote_results):
+    if isinstance(vote_results, dict):
+        print_dict(vote_results)
+    else:
+        print(vote_results)
+
+def print_dict(vote_results):
+    for i in vote_results:
+        print(i,":", vote_results[i])
+
 if __name__ == "__main__":
     voters = load_csv("data/profil1.csv")   
     result = vote.two_turn_vote(voters)
-    print(result)
+    print_result(result)
--- vote.py
+++ vote.py
@@ -30,12 +30,21 @@ def one_turn_vote(voters):
     return order_results(get_votes_distribution(voters))
 
 def two_turn_vote(voters, candidates_number=2):
+    results = dict()
     turn_1 = one_turn_vote(voters)
+    results[f'turn_1'] = turn_1
     eliminated = [x[0]-1 for i, x in enumerate(turn_1) if i>=candidates_number-1]
-    return order_results(get_votes_distribution(voters, eliminated))
+    results[f'turn_2'] =  order_results(get_votes_distribution(voters, eliminated))
+    return results
 
 
-# def alternative_vote(voters):
-#     result = []
-#     for vote in voters:
-#         turn = get_votes_distribution(voters, r)
+def alternative_vote(voters):
+    results = {}
+    number_of_candidates = len(voters[0])
+    eliminated = []
+    for i in range(number_of_candidates):
+        turn = order_results(get_votes_distribution(voters, eliminated))
+        eliminated.append(turn[-1][0])
+        results[f'turn_{i}']=turn
+        # print(eliminated)
+    return results
