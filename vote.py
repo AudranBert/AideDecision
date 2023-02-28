10c9bff231eda1b75c1fc3a01d3088a8c44e324b
Audran Bert <57795315+AudranBert@users.noreply.github.com>
2023-02-28 11:53:32 +0100

    add: second turn vote

--- vote.py
+++ vote.py
@@ -16,7 +16,8 @@ def get_votes_distribution(voters, elimineted=[], reverse=False):
 def order_results(vote_distribution_results):
     results = []
     for i, votes in enumerate(vote_distribution_results):
-        results.append((i+1, votes))
+        if votes>0:
+            results.append((i+1, votes))
     return sorted(
         results, 
         key=lambda x: x[1],
@@ -28,10 +29,10 @@ def order_results(vote_distribution_results):
 def one_turn_vote(voters):
     return order_results(get_votes_distribution(voters))
 
-def two_turn_vote(voters):
+def two_turn_vote(voters, candidates_number=2):
     turn_1 = one_turn_vote(voters)
-    eliminated = [x[0]-1 for i, x in enumerate(turn_1) if i>=2]
-    print(eliminated)
+    eliminated = [x[0]-1 for i, x in enumerate(turn_1) if i>=candidates_number-1]
+    return order_results(get_votes_distribution(voters, eliminated))
 
 
 # def alternative_vote(voters):
